c4228a0c8423659e77449b9fc784be45a6152b8e
audreyantony <webdev2020.audrey@gmail.com>
2020-06-06 22:49:09 +0200

    Mise au propre des commentaires

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ window.onload = function(){
 
     init();
 
+    // FONCTION D'INITIALISATION DU CANVAS ET CRÉATION DES ÉLÉMENTS
     function init(){
         let canvas = document.createElement('canvas');
         canvas.width = canvasWidth;
@@ -31,11 +32,14 @@ window.onload = function(){
         movementCanvas();
     }
 
+    // FONCTION DES DIFFÉRENTS MOUVEMENTS ET AFFICHAGES SUR LE CANVAS
     function movementCanvas() {
         theSnake.advance();
+        // CONDITION DE COLLISION
         if (theSnake.collision()){
             gameOver();
         } else {
+            // CONDITION D'AGRANDISSEMENT DU SERPENT
             if (theSnake.eatingFood(theFood)){
                 score ++;
                 theSnake.ateTheFood = true;
@@ -51,6 +55,7 @@ window.onload = function(){
         }
     }
 
+    // FONCTION AFFICHAGE DU GAME OVER
     function gameOver(){
         ctx.save();
         ctx.font = "bold 100px sans-serif";
@@ -70,6 +75,7 @@ window.onload = function(){
         ctx.restore();
     }
 
+    // FONCTION START OVER
     function resume(){
         theSnake = new snake([[6,4],[5,4],[4,4],[3,4],[2,4]], 'right');
         theFood = new food([10,10]);
@@ -77,7 +83,8 @@ window.onload = function(){
         clearTimeout(timeOut);
         movementCanvas();
     }
-    
+
+    // FONCTION D'AFFICHAGE DU SCORE
     function showScore() {
         ctx.save();
         ctx.font = "bold 50px sans-serif";
@@ -92,10 +99,12 @@ window.onload = function(){
         ctx.fillRect(x,y,blockSnake, blockSnake);
     }
 
+    // FONCTION DE LA CRÉATION DU SERPENT ET DE SES ACTIONS
     function snake(body, direction){
         this.body = body;
         this.direction = direction;
         this.ateTheFood = false;
+        // AFFICHAGE
         this.draw = function () {
             ctx.save();
             ctx.fillStyle = "#4F6742";
@@ -104,6 +113,7 @@ window.onload = function(){
             }
             ctx.restore();
         };
+        // MOUVEMENT
         this.advance = function () {
             let nextPosition = this.body[0].slice();
             switch (this.direction) {
@@ -123,12 +133,14 @@ window.onload = function(){
                     throw("La direction n'est pas valide");
             }
             this.body.unshift(nextPosition);
+            // L'ARRAY POP SEULEMENT SI LE SERPENT N'A PAS MANGÉ
             if (!this.ateTheFood){
                 this.body.pop();
             } else {
                 this.ateTheFood = false;
             }
         };
+        // DIRECTION DU MOUVEMENT
         this.setDirection = function(directionSnake){
             let directionValide;
             switch (this.direction) {
@@ -147,13 +159,14 @@ window.onload = function(){
                 this.direction = directionSnake;
             }
         };
+        // TEST COLLISION MUR OU SUR LUI MÊME
         this.collision = function(){
-            var collisionMur = false;
-            var collisionSnake = false;
+            let collisionMur = false;
+            let collisionSnake = false;
             let snakeHead = this.body[0];
-            var snakeBody = this.body.slice(1);
-            var snakeHeadX = snakeHead[0];
-            var snakeHeadY = snakeHead[1];
+            let snakeBody = this.body.slice(1);
+            let snakeHeadX = snakeHead[0];
+            let snakeHeadY = snakeHead[1];
             let minX = 0;
             let minY = 0;
             let maxX = widthBlockSnake - 1;
@@ -172,14 +185,17 @@ window.onload = function(){
             }
             return collisionSnake || collisionMur;
         };
+        // EST CE QU'IL MANGE
         this.eatingFood = function(theGodDamnFood){
             let head = this.body[0];
             return head[0] === theGodDamnFood.position[0] && head[1] === theGodDamnFood.position[1];
         };
     }
 
+    // FONCTION D'AFFICHAGE ET DES ACTIONS DE LA NOURRITURE DU SERPENT
     function food(position){
         this.position = position;
+        // AFFICHAGE
         this.draw = function () {
             ctx.save();
             ctx.fillStyle = "#a61717";
@@ -191,11 +207,13 @@ window.onload = function(){
             ctx.fill();
             ctx.restore();
         };
+        // RENOUVELLEMENT DE LA POSITION QUAND LA NOURRITURE EST MANGÉ
         this.otherFoodPosition = function() {
             let newX = Math.round(Math.random() * (widthBlockSnake - 1));
             let newY = Math.round(Math.random() * (heightBlockSnake - 1));
             this.position = [newX,newY];
         };
+        // LE RENOUVELLEMENT NE TOMBE PAS SUR LE SERPENT
         this.foodOnSnake = function(checkTheSnake){
             let foodOnSnake = false;
             for (let i = 0; i < checkTheSnake.body.length;i++){
@@ -208,6 +226,7 @@ window.onload = function(){
 
     }
 
+    // LIEN AVEC LES TOUCHES DIRECTIONNELLES DU CLAVIER
     document.onkeydown = function toucheUtil(e){
         let touche = e.key;
         switch (touche) {
