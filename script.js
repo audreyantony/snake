40f7702198a2411adf9fce3304a9f8f2eb5f40e5
audreyantony <webdev2020.audrey@gmail.com>
2020-06-06 09:48:48 +0200

    Dessin du serpent, triage des var

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 window.onload = function(){
 
-    var canvas;
+    var canvasWidth = 900;
+    var canvasHeight = 600;
+    var blockSnake = 30;
     var ctx;
     var delay = 100;
-    var xPos = 0;
-    var yPos = 0;
+    var theSnake;
 
     init();
 
     function init(){
-        canvas = document.createElement('canvas');
-        canvas.width = 900;
-        canvas.height = 600;
-        canvas.style.border = ' 2px solid';
+        let canvas = document.createElement('canvas');
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+        canvas.style.border = '2px solid';
         document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
+        theSnake = new snake([[6,4],[5,4],[4,4]]);
         movementSnake();
     }
 
     function movementSnake() {
-        xPos += 2;
-        yPos += 2;
-        ctx.clearRect(0,0,canvas.width, canvas.height);
-        ctx.fillStyle ="#ff0000";
-        ctx.fillRect(xPos,yPos,100,50);
+        ctx.clearRect(0,0,canvasWidth, canvasHeight);
+        theSnake.draw();
         setTimeout(movementSnake, delay);
+    }
+
+    function drawBlockSnake(ctx, position){
+        let x = position[0] * blockSnake;
+        let y = position[1] * blockSnake;
+        ctx.fillRect(x,y,blockSnake, blockSnake);
+    }
+
+    function snake(body){
+        this.body = body;
+        this.draw = function () {
+            ctx.save();
+            ctx.fillStyle = "#4F6742";
+            for(let i = 0; i < this.body.length ; i++){
+                drawBlockSnake(ctx, this.body[i])
+            }
+            ctx.restore();
+        };
     }
 };
